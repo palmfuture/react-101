a4f0fdb21f176e0c0acf524409951261964053c3
Wisit Srimala <palm.future@gmail.com>
2021-05-25 00:03:08 +0700

    add events

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ const HelloWorld = (props) => <div>Hello {props.name}</div>;
 const App = () => {
   const [name, setName] = useState("Palm .w");
 
+  const handleClick = () => {
+    setName("Meow Meow");
+  };
+
   return (
     <div>
       <HelloWorld name={name} />
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 };
