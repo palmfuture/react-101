77a8463718e111bcabcbdd0c2e90483436453a29
Wisit Srimala <palm.future@gmail.com>
2021-05-25 00:00:45 +0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 const HelloWorld = (props) => <div>Hello {props.name}</div>;
 
 const App = () => {
+  const [name, setName] = useState("Palm .w");
+
   return (
     <div>
-      <HelloWorld name="Palm w." />
+      <HelloWorld name={name} />
     </div>
   );
 };
